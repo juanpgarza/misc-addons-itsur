693a9ff8901c3c7b70d385d83e5b68f5c29a1101
Juan P Garza <juanp@juanpgarza.com>
2021-05-27 09:31:46 -0300

    [12.0][ADD] mail_restrict_external_partner_follower: creación inicial

---
+++ mail_restrict_external_partner_follower/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 ITSur - Juan Pablo Garza <jgarza@itsur.com.ar>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "mail_restrict_external_partner_follower",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Social",
+    "website": "https://github.com/itsurnqn/misc-addons",
+    "author": "ITSur",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "data": [
+        ],
+    "installable": True,
+}
